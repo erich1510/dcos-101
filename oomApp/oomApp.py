6da426b5e60ae34bf397eb3afbdd72edd7c36df9
Eric Huang <ehuang@macalester.edu>
2017-06-06 16:09:15 -0500

    Staged Changes Intiial commit

---
+++ oomApp/oomApp.py
@@ -0,0 +1,12 @@
+
+import time
+
+storage = {};
+for i in range(1, 100):
+  big_str = ' ' * 1000000
+  storage[i] = some_str
+  time.sleep(0.1)
+  print "Eat 1 MB"
+
+print "Satisfied!"
+time.sleep(10)
